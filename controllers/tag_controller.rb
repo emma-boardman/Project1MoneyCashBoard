37e3ba1b5ba0a38381ae613be7ce8cd855bdbf7d
Emma Boardman <emma@emmaboardman.co.uk>
2016-08-16 12:02:31 +0100

    flex boxes working

--- controllers/tag_controller.rb
+++ controllers/tag_controller.rb
@@ -3,5 +3,6 @@ require_relative('../models/tag')
 #index
 get '/tags' do
   @tags = Tag.all
+  @analysis = Analysis.new()
   erb(:'tags/index')
 end
--- main_controller.rb
+++ main_controller.rb
@@ -5,5 +5,6 @@ require_relative('controllers/tag_controller')
 require_relative('controllers/transaction_controller')
 
 get '/' do
+  @analysis = Analysis.new()
   erb :home
 end
