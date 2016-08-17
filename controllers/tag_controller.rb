113a3a5df06aa7b79adff11360012b3de90b0512
Emma Boardman <emma@emmaboardman.co.uk>
2016-08-17 10:34:45 +0100

    added route for adding new tags

--- controllers/tag_controller.rb
+++ controllers/tag_controller.rb
@@ -6,3 +6,17 @@ get '/tags' do
   @analysis = Analysis.new()
   erb(:'tags/index')
 end
+
+
+#new
+get '/tags/new' do
+  @tags = Tag.all
+  erb (:'tags/new')
+end
+
+#create
+post '/transactions' do
+  transaction = Transaction.new(params) 
+  transaction.save()
+  redirect( to('transactions') )
+end
