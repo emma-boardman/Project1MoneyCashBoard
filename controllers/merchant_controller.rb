08c2e2c8cc4935e9cc881fc1852d592c1c76001f
Emma Boardman <emma@emmaboardman.co.uk>
2016-08-17 10:45:59 +0100

    new and create routes added to merchant controller

--- controllers/merchant_controller.rb
+++ controllers/merchant_controller.rb
@@ -5,3 +5,17 @@ get '/merchants' do
   @merchants = Merchant.all
   erb(:'merchants/index')
 end
+
+
+#new
+get '/merchants/new' do
+  @merchants = Merchant.all
+  erb (:'merchants/new')
+end
+
+#create
+post '/merchants' do
+  merchant = Merchant.new(params) 
+  merchant.save()
+  redirect( to('merchants') )
+end
